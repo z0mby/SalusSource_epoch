e6f698071ba0f5114baefa940662cb4d06ca5ad0
dabyland <dabyland1002@gmail.com>
2014-11-05 13:28:41 -0500

    Added one more button

--- ProjectDraft/Menu.Designer.cs
+++ ProjectDraft/Menu.Designer.cs
@@ -37,6 +37,7 @@
             this.addPatientbtn = new System.Windows.Forms.Button();
             this.patientDBbtn = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -52,6 +53,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.exitBtn);
             this.groupBox1.Controls.Add(this.manualBtn);
@@ -133,6 +135,15 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(111, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Test2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -162,5 +173,6 @@
         private System.Windows.Forms.Button addPatientbtn;
         private System.Windows.Forms.Button patientDBbtn;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
